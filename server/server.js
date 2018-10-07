c23cc417fdd1a6c30b2022735112d6f8824fad4c
Deb <m.debabrata100@gmail.com>
2018-10-07 19:17:06 +0530

    Emit new Message on createMessage

--- server/server.js
+++ server/server.js
@@ -20,6 +20,11 @@ io.on('connection',(socket)=>{
     })
     socket.on("createMessage",(message)=>{
         console.log("createMessage",message );
+        io.emit("newMessage",{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
     socket.on("disconnect",()=>{
         console.log("User is disconnected");
