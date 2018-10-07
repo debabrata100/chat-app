58e05f5a912f0bbb080ad98d7277deb5b0971e6d
Deb <m.debabrata100@gmail.com>
2018-10-07 18:21:12 +0530

    Connection and Disconnection events added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+const io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log("New user connected");
+    socket.on("disconnect",()=>{
+        console.log("User is disconnected");
+    })
+})
+
+server.listen(port,()=>{
     console.log(`Server is on at ${port}`);
 });
