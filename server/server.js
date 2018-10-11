b0beac5950fd002014c428b1f9aebb31500420b7
Deb <m.debabrata100@gmail.com>
2018-10-11 22:08:55 +0530

    brodcasting messages to other users when a new user joins

--- server/server.js
+++ server/server.js
@@ -11,11 +11,12 @@ const server = http.createServer(app);
 app.use(express.static(publicPath));
 
 const io = socketIO(server);
+var count = 0;
 
 io.on('connection',(socket)=>{
     console.log("New user connected");
     socket.emit("newMessage",generateMessage("Admin","Welcome to the chat App!"));
-    socket.broadcast.emit("newMessage",generateMessage("Admin", "New user joined"));
+    socket.broadcast.emit("newMessage",generateMessage("Admin", "New user joined: "+(++count) ));
     socket.on("createMessage",(message)=>{
         console.log("createMessage",message );
         io.emit("newMessage",generateMessage(message.from,message.text))
